bcebe18b40cdcadbc42d9ea286dbfd1a2bf769ea
Justin <98992595+JLCurtsinger@users.noreply.github.com>
2025-05-06 15:52:08 -0700

    Update Podcasts.tsx
    added the 4th podcast episode

--- src/pages/Podcasts.tsx
+++ src/pages/Podcasts.tsx
@@ -84,6 +84,22 @@ export default function Podcasts() {
               />
             </div>
           </ScrollReveal>
+
+          <ScrollReveal>
+            <div className="max-w-4xl mx-auto mb-16">
+              <iframe 
+                style={{ borderRadius: "12px" }} 
+                src="https://open.spotify.com/embed/episode/3I9F8OeObx111FEQorzUdS?utm_source=generator&theme=0&t=0" 
+                width="100%" 
+                height="352" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="lazy"
+                title="The MedSafe Podcast Episode Four"
+              />
+            </div>
+          </ScrollReveal>
           
           <ScrollReveal>
             <div className="max-w-4xl mx-auto text-center">
