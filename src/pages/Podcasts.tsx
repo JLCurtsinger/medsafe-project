5e776cb2e19d7e28e16a91f7fde7f8258c26940d
Justin <98992595+JLCurtsinger@users.noreply.github.com>
2025-05-09 16:51:35 -0700

    Update Podcasts.tsx
    added season 2 episode 2

--- src/pages/Podcasts.tsx
+++ src/pages/Podcasts.tsx
@@ -148,6 +148,22 @@ export default function Podcasts() {
               />
             </div>
           </ScrollReveal>
+
+          <ScrollReveal>
+            <div className="max-w-4xl mx-auto mb-16">
+              <iframe 
+                style={{ borderRadius: "12px" }} 
+                src="https://open.spotify.com/embed/episode/63Z519Gobta6SxRr5fGp1J?utm_source=generator&theme=0&t=0" 
+                width="100%" 
+                height="352" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="lazy"
+                title="The MedSafe Podcast Season Two, Episode Two"
+              />
+            </div>
+          </ScrollReveal>
           
           <ScrollReveal>
             <div className="max-w-4xl mx-auto text-center">
