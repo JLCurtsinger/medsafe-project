707671d2435ec61f6441c19eb2018e91b2e177a9
Justin <98992595+JLCurtsinger@users.noreply.github.com>
2025-05-29 06:58:15 -0700

    Update articles.ts
    added mind the gap article

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -5,6 +5,7 @@ import latestResearch from './articles/latest-research-medication-harm';
 import technologyPrevention from './articles/technology-preventing-medication-errors';
 import olderAdultsConcerns from './articles/medication-concerns-older-adults';
 import personalMedicationRecord from './articles/create-personal-medication-record';
+import mindTheGap from './articles/mind-the-gap';
 
 export const articles = [
   understandingDrugInteractions,
@@ -12,5 +13,6 @@ export const articles = [
   latestResearch,
   technologyPrevention,
   olderAdultsConcerns,
-  personalMedicationRecord
+  personalMedicationRecord,
+  mindTheGap
 ];
